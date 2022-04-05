44e8b883b9e7305832ba4241ec55f204c3d4e3c1
rohanjarun <47125397+rohanjarun@users.noreply.github.com>
2022-04-05 16:46:31 -0700

    Final

--- src/A2/RollBounce.java
+++ src/A2/RollBounce.java
@@ -1,70 +1,145 @@
 package A2;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Random;
+//importing the packages required
 
-public class RollBounce extends JPanel implements ActionListener {
-
+public class RollBounce extends JPanel implements ActionListener { //declaring the parameters required for this assignment
     protected Timer tm;
-
-    // TODO: Consider removing the next two lines (coordinates for two balls)
+    private final Properties ballConfig;
+    private final ListNode<Ball> balls;
+    private final ListNode<Color> colors;
     protected int x1, y1;
     protected int x2, y2;
-
-
-    public RollBounce (String propertyFileName) {
-        // TODO: insert your code to read from configuration file here.
-
-        tm = new Timer(75, this); // TODO: Replace the first argument with delay with value from config file.
-
-        // TODO: Consider removing the next two lines (coordinates) for random starting locations.
-        x1 = 100; y1 = 50;
-        x2 = 200; y2 = 400;
+    private final int minSpeed;
+    private final int maxSpeed;
+    private int gravity;
+    private final int friction;
+    private final int width;
+    private final int height;
+    private final int radius;
+    public RollBounce (String propertyFileName) throws IOException { //gets the properties for the file
+        ballConfig = new Properties();
+        FileInputStream in = new FileInputStream(propertyFileName);
+        ballConfig.load(in);
+        tm = new Timer(Integer.parseInt(ballConfig.getProperty("timerDelay")), this);
+        minSpeed = Integer.parseInt(ballConfig.getProperty("minspeed"));
+        maxSpeed = Integer.parseInt(ballConfig.getProperty("maxspeed"));
+        gravity = Integer.parseInt(ballConfig.getProperty("gravity"));
+        friction = Integer.parseInt(ballConfig.getProperty("friction"));
+        height = Integer.parseInt(ballConfig.getProperty("window_height"));
+        width = Integer.parseInt(ballConfig.getProperty("window_width"));
+        radius = Integer.parseInt(ballConfig.getProperty("ball_radius"));
+        balls = new ListNode<>();
+        colors = new ListNode<>();
+        colors.add(Color.BLUE);
+        colors.add(Color.ORANGE);
+        colors.add(Color.MAGENTA);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.YELLOW);
+        for (int i = 0; i < Integer.parseInt(ballConfig.getProperty("balls")); i++) {
+            addBall();
+        }
     }
-
-
     public void paintComponent(Graphics g) {
-        super.paintComponent(g); // Probably best you leave this as is.
-
-        // TODO: Paint each ball. Here's how to paint two balls, one after the other:
-        g.setColor(Color.BLUE);
-        g.fillOval(x1, y1, 20, 20);
-
-        g.setColor(Color.RED);
-        g.fillOval(x2, y2, 20, 20);
-
-        // Recommend you leave the next line as is
+        super.paintComponent(g); // Sets the color of the ball
+        ListNode<Color> currentColorNode = colors.getNext();
+        Color currentColor;
+        ListNode<Ball> currentBallNode = balls.getNext();
+        Ball currentBall;
+        while (currentBallNode != null) {
+            if (currentColorNode == null) {
+                currentColorNode = colors.getNext();
+            }
+            currentBall = currentBallNode.getItem();
+            currentColor = currentColorNode.getItem();
+            g.setColor(currentColor);
+            g.fillOval(currentBall.getX(), currentBall.getY(), radius, radius);
+            currentBallNode = currentBallNode.getNext();
+            currentColorNode = currentColorNode.getNext();
+        }
         tm.start();
     }
-
-
+    public void addBall() { //adds the ball by first checking the minSpeed and the Max
+        Random random = new Random();
+        int initialVertical = random.nextInt(maxSpeed-minSpeed) + minSpeed;
+        int initialHorizontal = random.nextInt(maxSpeed-minSpeed) + minSpeed;
+        int initialX = random.nextInt(width);
+        int initialY = random.nextInt(height);
+        Ball newBall = new Ball(initialX, initialY, initialVertical, initialHorizontal);
+        balls.add(newBall);
+    }
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        // TODO: Change the location of each ball. Here's an example of them moving across the screen:
-        //       ... but to be clear, you should change this.
-        x1 += 10;
-        x2 -= 15;
-        // These two "if" statements keep the balls on the screen in case they go off one side.
-        if (x1 > 640)
-            x1 = 0;
-        if (x2 < 0)
-            x2 = 640;
 
-        // Keep this at the end of the function (no matter what you do above):
+        ListNode<Ball> currentBallNode = balls.getNext(); //checks if the current ball is within the parameters of width and radius
+        Ball currentBall;
+        while (currentBallNode != null) {
+            currentBall = currentBallNode.getItem();
+            currentBall.setX(currentBall.getX() + currentBall.getHorizontalVelocity());
+            if (currentBall.getX() <= 0) {
+                currentBall.setX(0);
+                slowBall(currentBall);
+                currentBall.setHorizontalVelocity(-currentBall.getHorizontalVelocity());
+            } else if (currentBall.getX() >= width - 2*radius) {
+                currentBall.setX(width-2*radius);
+                slowBall(currentBall);
+                currentBall.setHorizontalVelocity(-currentBall.getHorizontalVelocity());
+            }
+            currentBall.setY(currentBall.getY() + currentBall.getVerticalVelocity());
+            if (currentBall.getY() >= height-3*radius) {
+                currentBall.setY(height-3*radius);
+                slowBall(currentBall);
+                currentBall.setVerticalVelocity(-Math.abs(currentBall.getVerticalVelocity()));
+            }
+            currentBall.setVerticalVelocity(currentBall.getVerticalVelocity()+gravity);
+            currentBallNode = currentBallNode.getNext();
+        }
         repaint();
     }
 
-    public static void main(String[] args) {
-        RollBounce rb = new RollBounce(args[0]);
+    private void slowBall(Ball ball) {
+        int verticalVelocity = ball.getVerticalVelocity(); //gets the int value of the vertical ball velocity
+        int horizontalVelocity = ball.getHorizontalVelocity();
+        if (Math.abs(verticalVelocity) < friction) {
+            verticalVelocity = 0;
+        } else if (verticalVelocity > 0) {
+            verticalVelocity -= friction;
+        } else {
+            verticalVelocity += friction;
+        }
+        if (Math.abs(horizontalVelocity) < friction) {
+            horizontalVelocity = 0;
+        } else if (horizontalVelocity > 0) {
+            horizontalVelocity -= friction;
+        } else {
+            horizontalVelocity += friction;
+        }
+        ball.setVerticalVelocity(verticalVelocity);
+        ball.setHorizontalVelocity(horizontalVelocity);
+    }
+    public static void main(String[] args) { //main class to run the file
+        try {
+            RollBounce rb = new RollBounce(args[0]);
+            JFrame jf = new JFrame();
+            jf.setTitle("Roll Bounce");
+            jf.setSize(rb.width, rb.height);
+            jf.add(rb);
+            jf.setVisible(true);
+            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        } catch (FileNotFoundException e) { //added the exception when filenotfound
+            System.out.println("Invalid file argument");
+        } catch (IOException e) {
+            System.out.println("Invalid properties file");
+        }
 
-        JFrame jf = new JFrame();
-        jf.setTitle("Roll Bounce");
-        jf.setSize(640, 480); // TODO: Replace with the size from configuration!
-        jf.add(rb);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
